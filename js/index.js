90ee5fc25cec3827493236a1ba9586deeb40afc3
Matthew Croak <matthewcroak@Matthews-MBP.home>
2017-12-18 10:33:57 -0500

    working on qr code

--- js/index.js
+++ js/index.js
@@ -2,11 +2,23 @@ let outer = document.getElementsByClassName("outerModal")[0]
 let interviewModalButton = document.getElementsByClassName("interviewModalButton")[0]
 
 document.addEventListener('DOMContentLoaded', function(){
-  let workWithUs = document.getElementById("modalButton")
+  let workWithUs = document.getElementById("workWithUsModalButton")
   workWithUs.addEventListener("click", showSignUpInterviewModal)
+  subscribeButton.addEventListener("click", showMailingListModal)
 })
 
 function showSignUpInterviewModal(){
+  debugger
+   outer.style.display="unset"
+   document.getElementsByClassName("signUpOrInterviewModal")[0].style.display="unset"
+   document.getElementsByClassName("signUpModalButton")[0].style.display="unset"
+   interviewModalButton.style.display="unset"
+   interviewModalButton.addEventListener("click", closeSignUpInterviewModal)
+   console.log(outer)
+   outer.addEventListener("click", closeSignUpInterviewModal)
+}
+
+function showMailingListModal(){
   debugger
    outer.style.display="unset"
    document.getElementsByClassName("signUpOrInterviewModal")[0].style.display="unset"
