078e77e16b3b0cfaa8e1e298a4070eb5c7a3fb4a
Matthew Croak <matthewcroak@Matthews-MBP.home>
2017-12-19 10:29:28 -0500

    done

--- js/events.js
+++ js/events.js
@@ -6,6 +6,7 @@ let lastInput;
 let phoneInput;
 let workWithUs;
 let license;
+let ticket;
 let outer = document.getElementsByClassName("outerModal")[0]
 let rsvp = document.getElementsByClassName("rsvpForm")[0]
 document.addEventListener('DOMContentLoaded', function(){
@@ -43,11 +44,6 @@ function openEventForm(data){
   rsvp.style.display = "unset"
   outer.addEventListener("click", closeRsvpFormModal)
   rsvp.addEventListener("submit", submitCompanyLeadRsvpFormData)
-}
-
-function closeRsvpFormModal(){
-  outer.style.display="none"
-  rsvp.style.display="none"
 }
 
 function submitCompanyLeadRsvpFormData(e){
@@ -92,11 +88,18 @@ function showTicket(data){
   rsvp.style.display = "none"
   debugger
   document.getElementById('ticketTitleHeader').innerHTML = data.ticket.title
-  document.getElementById('ticketDateHeader').innerHTML = data.ticket.date
-  document.getElementById('ticketConfirmationHeader').innerHTML = data.ticket.confirmation
-  document.getElementById('ticketLocationHeader').innerHTML = data.ticket.location
+  document.getElementById('ticketDateHeader').innerHTML = "when: " + data.ticket.date
+  document.getElementById('ticketLocationHeader').innerHTML = "where: " + data.ticket.location
   let qrCodeRendered = document.getElementById('placeHolder').innerHTML = data.qrCode.createImgTag();
+  document.getElementById('ticketConfirmationHeader').innerHTML = "confirmation: " + data.ticket.confirmation
   debugger
-  qrCodeRendered.style.width = "30px"
-  document.getElementById('ticket').style.display = "unset"
+  //
+  ticket = document.getElementById('ticket').style.display = "unset"
+}
+
+function closeRsvpFormModal(){
+  outer.style.display="none"
+  rsvp.style.display="none"
+  ticket.style.display="none"
+
 }
