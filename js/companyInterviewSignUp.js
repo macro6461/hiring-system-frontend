1de218d1851aec85853344397cfd4d946076dc98
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-08 18:30:33 -0500

    cleaned up form submissions, working on trainer lead interviews in backend

--- js/companyInterviewSignUp.js
+++ js/companyInterviewSignUp.js
@@ -10,9 +10,13 @@ document.addEventListener('DOMContentLoaded', function(){
   let phoneInput = document.getElementById("phoneInput")
   let workWithUs = document.getElementById("modalButton")
   let license = document.querySelector('input[name="licensed"]')
+  let trainerInput = document.getElementById("trainerInput")
   interviewForm.addEventListener("submit", submitCompanyLeadInterviewFormData)
   console.log(license)
 })
+
+//MAKE ACTION MAILER IN BACKEND TO SEND CONFIRMATION COMPANY/TRAINER LEAD INTERVIEW
+//ANDRES WILL DETERMINE THE DATES WITH HIS BACKEND
 
 function submitCompanyLeadInterviewFormData(e){
   e.preventDefault()
@@ -20,18 +24,25 @@ function submitCompanyLeadInterviewFormData(e){
   debugger
   console.log(licensed)
   event.preventDefault()
-  fetch("http://localhost:3000/company_lead_interviews", {
-        headers: {"Content-Type": "application/json",
-        "Accept":"application/json"},
-        method: "POST",
-        body: JSON.stringify({
-          email_address: emailInput.value,
-          first_name: firstInput.value,
-          last_name: lastInput.value,
-          phone_number: phoneInput.value,
-          licensed: licensed.value,
+  if (trainerInput.length > 0){
+    //send post request to trainers, find trainer, get trainer id, then make post request to trainer_lead_interviews, create
+    //trainer_lead_interview, send confirmation email
+  } else {
+    fetch("http://localhost:3000/company_lead_interviews", {
+          headers: {"Content-Type": "application/json",
+          "Accept":"application/json"},
+          method: "POST",
+          body: JSON.stringify({
+            email_address: emailInput.value,
+            first_name: firstInput.value,
+            last_name: lastInput.value,
+            phone_number: phoneInput.value,
+            licensed: licensed.value,
+          })
         })
-      })
-      .then(res => res.json())
-      .then(json => console.log(json))
+        .then(res => res.json())
+        .then(json => console.log(json))
+        //send confirmation email
+  }
+
 }
